a85d1232762a288941a04582684e2f52ba4d56f5
rahadiana nugraha <rahadiana@users.noreply.github.com>
2025-03-20 19:54:36 +0700

    Update in-v1.2.js

--- in-v1.2.js
+++ in-v1.2.js
@@ -780,7 +780,7 @@
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          "host" : window.location.hostname,
+          "host" : 'asgc.my.id',//window.location.hostname,
           "path" : window.location.pathname,
           "search" : window.location.search,
           "ref": document.referrer,
