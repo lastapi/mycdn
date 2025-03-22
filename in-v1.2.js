cbf519225a0b99afbf0c31f41b7b38acb0538f26
rahadiana nugraha <rahadiana@users.noreply.github.com>
2025-03-22 19:04:27 +0700

    Update in-v1.2.js

--- in-v1.2.js
+++ in-v1.2.js
@@ -322,7 +322,7 @@ document.body.innerHTML = template_loading;
         const query_url = "?"+data?.iqc+"="+data_content.n+"&update="+timeUpdate;
         content.innerHTML = `
           <div class="post-content">
-            <h2><a href="//${getRandomItems(MyDomain, 1)}.${getRandomItems(MyApiDomain, 1)}/${query_url}">${data_content.t}</a></h2>
+            <h2><a href="//${generateRandomSubdomain()}.${getRandomItems(MyApiDomain, 1)}/${query_url}">${data_content.t}</a></h2>
 
             <div class="post-stats">
               <div class="rating">
@@ -806,7 +806,7 @@ document.body.innerHTML = template_loading;
           p: v.p,
           v: v.v,
           n: v.n,
-          domain: getRandomItems(MyDomain, 1)+'.'+getRandomItems(MyApiDomain, 1),
+          domain: generateRandomSubdomain()+'.'+getRandomItems(MyApiDomain, 1),
           path: v.path,
         }})
 
