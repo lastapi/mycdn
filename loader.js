316a4f3d7483bb15b45cc594fa9b24f95058170c
rahadiana nugraha <rahadiana@users.noreply.github.com>
2025-04-06 04:00:25 +0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -206,11 +206,11 @@ async function fetchData(data) {
     }
 
     if (results.length === 10) {
-        domains.forEach(d => {
-            updateContent(d.lang, (results[2].data.questionById.content).split(" ")[0]);
-            updateContent(d.lang, (results[1].data.questionById.content).split(" ")[1]);
-            updateContent(d.lang, (results[0].data.questionById.content).split(" ")[2]);
-        });
+        // domains.forEach(d => {
+        //     updateContent(d.lang, (results[2].data.questionById.content).split(" ")[0]);
+        //     updateContent(d.lang, (results[1].data.questionById.content).split(" ")[1]);
+        //     updateContent(d.lang, (results[0].data.questionById.content).split(" ")[2]);
+        // });
 
         return results.map(w => {
             const ii = w.data.questionById;
