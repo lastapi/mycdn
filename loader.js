d422967c19b1388930fe42c54a8b303e9ae50470
rahadiana nugraha <rahadiana@users.noreply.github.com>
2025-04-12 22:25:18 +0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -189,7 +189,8 @@ async function fetchWithRetry(url, retries = 13, delay = 500) {
 
 async function fetchData(data) {
     const results = [];
-
+    const GenRand = data.files.map(x=>getRandomItems(x,1));
+    console.log(GenRand)
     for (const file of data.files) {
         const match = file.match(/(\d+)\.json$/);
         if (!match) continue;
